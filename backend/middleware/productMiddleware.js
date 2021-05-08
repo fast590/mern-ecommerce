2725682a4ba5bc889c111e42f2db42cd400fb5d3
WangDona <develope0407@gmail.com>
2021-05-08 00:33:46 -0700

    frontend

--- backend/middleware/productMiddleware.js
+++ backend/middleware/productMiddleware.js
@@ -1,19 +1,21 @@
 const express = require('express')
 const Product = require('../models/product')
 
-const getAllProducts = (req, res, next) => {
-    const products = Product
-        .find({})
-        .then(() => console.log())
-        .catch(err => console.log(err))
-    res.json(products);
+const getAllProducts = (req, res) => {
+    Product
+        .find({}, (err, data) => {
+            if(err) console.log(err)
+            if(data) res.json({products: data});
+            else res.json({products: 'dont exist'})
+        })
 }
-const getProductById = (req, res, next) => {
-    const product = Product
-        .find(req.body.id)
-        .then(() => console.log())
-        .catch(err => console.log(err))
-    res.json(product);
+const getProductById = (req, res) => {
+    Product
+        .find(req.body.id, (err, data) => {
+            if(err) console.log(err)
+            if(data) res.json({product: data});
+            else res.json({product: 'dont exist'})
+        })
 }
 
 module.exports = {
